5cd0b5c315e059af3ea10e65d454c8a7cc5eb745
Harris Mariano <harris.mariano@adish.co.jp>
2021-11-02 12:25:16 +0800

    Added seeder for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+def seed_image(file_name)
+  File.open(File.join("#{Rails.root}/app/assets/images/#{file_name}"))
+end
+
+Movie.destroy_all
+10.times do |i|
+  Movie.create(
+    title: "Movie #{i + 1}",
+    release_year: rand(2020..2030).to_s,
+    genre: 'Action',
+    director: 'Harris Mariano',
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '\
+             'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '\
+             'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip '\
+             'ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum '\
+             'dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui '\
+             'officia deserunt mollit anim id est laborum.',
+    image: seed_image('spiderman.jpg')
+  )
+end
