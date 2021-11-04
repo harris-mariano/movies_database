135362247fb4238a1f38ea2725b557f2a2a36ca7
Harris Mariano <harris.mariano@adish.co.jp>
2021-11-04 15:40:37 +0800

    Added spec tests for requests

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :review do
-    movie
-    user
     comment { 'Sample comment' }
     score { 5 }
   end
---
+++ spec/requests_helper.rb
@@ -0,0 +1,8 @@
+def login(user)
+  post user_session_path, params: {
+    user: {
+      email: user.email, password: user.password
+    }
+  }
+  follow_redirect!
+end
