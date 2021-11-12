beac4968891b2d1bca2f4a262d41bd5d64e8fb5a
Harris Mariano <harris.mariano@adish.co.jp>
2021-11-12 11:18:15 +0800

    Updated test specs for routes

--- spec/routing/reviews_routing_spec.rb
+++ spec/routing/reviews_routing_spec.rb
@@ -2,37 +2,26 @@ require "rails_helper"
 
 RSpec.describe ReviewsController, type: :routing do
   describe "routing" do
-    it "routes to #index" do
-      expect(get: "/reviews").to route_to("reviews#index")
-    end
+    let(:review) { create(:review, user: user, movie: movie) }
 
-    it "routes to #new" do
-      expect(get: "/reviews/new").to route_to("reviews#new")
-    end
-
-    it "routes to #show" do
-      expect(get: "/reviews/1").to route_to("reviews#show", id: "1")
+    it "routes to #create" do
+      expect(post: "/movies/1/reviews").to route_to("reviews#create", "movie_id"=>"1")
     end
 
     it "routes to #edit" do
-      expect(get: "/reviews/1/edit").to route_to("reviews#edit", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/reviews").to route_to("reviews#create")
+      expect(get: "/movies/1/reviews/1/edit").to route_to("reviews#edit", id: "1", "movie_id"=>"1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/reviews/1").to route_to("reviews#update", id: "1")
+      expect(put: "/movies/1/reviews/1").to route_to("reviews#update", id: "1", "movie_id"=>"1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/reviews/1").to route_to("reviews#update", id: "1")
+      expect(patch: "/movies/1/reviews/1").to route_to("reviews#update", id: "1", "movie_id"=>"1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/reviews/1").to route_to("reviews#destroy", id: "1")
+      expect(delete: "/movies/1/reviews/1").to route_to("reviews#destroy", id: "1", "movie_id"=>"1")
     end
   end
 end
